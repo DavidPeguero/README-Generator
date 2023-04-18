a4a373826cdc571e173c09880cb81aa81e7e0c4b
David Peguero <david.peguero.cancel@gmail.com>
2023-04-18 14:48:26 -0400

    Added more questions to the readme

--- index.js
+++ index.js
@@ -4,9 +4,40 @@ const fs = require('fs')
 // TODO: Create an array of questions for user input
 const questions = [
     {
+        name : 'title', 
+        type : 'input',
+        message:'What is the title for your project?',
+    },
+    {
         name : 'description', 
         type : 'input',
         message:'What is the description for your project?',
+    },
+    {
+        name : 'installation', 
+        type : 'input',
+        message:'What are the installation instructions for your project?',
+    },
+    {
+        name : 'usage', 
+        type : 'input',
+        message:'What is the usage information for your project?',
+    },
+    {
+        name : 'cGuidelines', 
+        type : 'input',
+        message:'What are the contribution guidelines for your project?',
+    },
+    {
+        name : 'test', 
+        type : 'input',
+        message:'What are the test instructions for your project?',
+    },
+    {
+        name : 'license', 
+        type : 'list',
+        message:'What is the licence for your project?',
+        options: ['MIT', 'GNU', 'OTHER']
     }
 ];
 
